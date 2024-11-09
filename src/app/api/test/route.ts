48c3e6c39a0a3283f4fe7cf7ab8d76e06654383e
Station Chenqiao <i@cctv3.net>
2024-11-09 23:51:55 +0800

    + `create-next-app@latest` & /api/test --> GET/POST ...

---
+++ src/app/api/test/route.ts
@@ -0,0 +1,26 @@
+import { NextRequest, NextResponse } from "next/server";
+
+/**
+ * http://localhost:3000/api/test?name=Zhangsan&age=30
+ * @param reqest
+ * @returns
+ */
+export async function GET(reqest: NextRequest) {
+  let params = reqest.nextUrl.searchParams;
+  return NextResponse.json({
+    data: { name: params.get("name"), age: params.get("age") },
+  });
+}
+
+/**
+ * http://localhost:3000/api/test
+ * {"name": "Zhangsan", "age": "30"}
+ * @param reqest
+ * @returns
+ */
+export async function POST(reqest: NextRequest) {
+  let params = await reqest.json();
+  return NextResponse.json({
+    data: params,
+  });
+}
